3a62ccdafd91c81119ac828dff3b40467ba32b4d
Guka Mamadashvili <114259602+guka19@users.noreply.github.com>
2026-02-23 10:55:01 +0400

    Add index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,1 @@
-const express=require('express');const http=require('http');const {Server}=require('socket.io');const app=express();const server=http.createServer(app);const io=new Server(server,{cors:{origin:'*'}});io.on('connection',socket=>{});server.listen(4000);
+const express=require('express');const http=require('http');const {Server}=require('socket.io');const chatHandlers=require('./chatHandlers');const app=express();const server=http.createServer(app);const io=new Server(server,{cors:{origin:'*'}});chatHandlers(io);server.listen(4000);
